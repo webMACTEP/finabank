b7a74cc03206645387e7a6bfb763c8f33221c9b1
webMACTEP <70036631+webMACTEP@users.noreply.github.com>
2024-11-15 11:29:08 +0400

    07

--- js/script.js
+++ js/script.js
@@ -7,4 +7,28 @@ document.addEventListener('DOMContentLoaded', function () {
 
         });
     });
+
+     // Выбираем все элементы с классом 'item'
+     const items = document.querySelectorAll('.cards .items .item');
+
+     items.forEach(function(item) {
+         const openBtn = item.querySelector('.open');
+         const closeBtn = item.querySelector('.close');
+         const textBlock = item.querySelector('.text');
+ 
+         // Обработчик для кнопки 'open'
+         openBtn.addEventListener('click', function() {
+             openBtn.classList.remove('active');
+             closeBtn.classList.add('active');
+             textBlock.classList.add('active');
+         });
+ 
+         // Обработчик для кнопки 'close'
+         closeBtn.addEventListener('click', function() {
+             closeBtn.classList.remove('active');
+             textBlock.classList.remove('active');
+             openBtn.classList.add('active');
+         });
+     });
 });
+
